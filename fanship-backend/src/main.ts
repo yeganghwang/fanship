5de35bdb90a4eb94d08ab2a428a95a64d7dd0c6b
yeganghwang <ghkdtlahs@icloud.com>
2025-07-28 17:34:39 +0900

    cors 설정

--- fanship-backend/src/main.ts
+++ fanship-backend/src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정 추가
+  app.enableCors({
+    origin: 'http://localhost:3001', // 프론트엔드 애플리케이션의 Origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector))); // Add ClassSerializerInterceptor
