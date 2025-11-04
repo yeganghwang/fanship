0a49bbce7b2b478cdb5235d1c2ef78c4c84369de
yeganghwang <101787202+yeganghwang@users.noreply.github.com>
2025-11-04 13:36:57 +0900

    UPDATE: enhance JWT module configuration and update package dependencies

--- fanship-backend/src/auth/auth.module.ts
+++ fanship-backend/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -17,10 +17,14 @@ import { Company } from '../company/company.entity';
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
-      }),
+      useFactory: async (configService: ConfigService): Promise<JwtModuleOptions> => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { 
+            expiresIn: configService.get('JWT_EXPIRES_IN') || '1h'
+          },
+        };
+      },
       inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User, Company]),
